644306c040caa33e4e949a686819706a7a306d38
cosplex01 <cosplex01@naver.com>
2019-06-26 15:54:32 +0900

    실수값 입력처리

---
+++ study/day3/exam3/exam3.cpp
@@ -0,0 +1,18 @@
+﻿// exam3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <stdio.h>
+
+int main()
+{
+	double num1, num2, result;
+	printf_s("두개의 수를 입력하시오");
+	scanf_s("%lf %lf", &num1, &num2);
+
+	result = (num1 + num2) / 2.0;
+
+	printf_s("결과는 :%lf\n",result);
+
+	return 0;
+}
