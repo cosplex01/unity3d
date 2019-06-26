6d41e69b2faf415fc2f5a058d6d47db38d8318fc
cosplex01 <cosplex01@naver.com>
2019-06-26 16:35:29 +0900

    소수점 입력하여 계산하기

--- study/day3/exam3/exam3.cpp
+++ study/day3/exam3/exam3.cpp
@@ -6,13 +6,30 @@
 
 int main()
 {
-	double num1, num2, result;
-	printf_s("두개의 수를 입력하시오");
-	scanf_s("%lf %lf", &num1, &num2);
+	int gibonyogum = 660;
+	double sayongyogum = 88.5;
+	double zoncheyogum = 0.0;
+	int sayongyang = 1;
+	double segum = 0.0;
+	double gojiseryogum = 0.0;
 
-	result = (num1 + num2) / 2.0;
+	//printf_s("두개의 수를 입력하시오");
+	printf_s("전기사용량을 입력하세요(kw)");
+	scanf_s("%d", &sayongyang); // 사용량 확인
 
-	printf_s("결과는 :%lf\n",result);
+	zoncheyogum = gibonyogum + (sayongyang * sayongyogum); //전체요금 계산
+	segum = zoncheyogum * 0.09; // 세금계산
+
+	gojiseryogum = zoncheyogum + segum;
+	printf_s("전기사용요금은 %lf 원 입니다\n", gojiseryogum);
+
+	//result = (num1 + num2) / 2.0;
+
+	//printf_s("결과는 :%lf\n",result);
+
+//	printf_s("결과는 :%d\n", result);
+
+	//printf_s("결과는 :%ld\n", ((int)(&num1) - (int)(&num2)));
 
 	return 0;
 }
