9ab589bfacbd3fd45d5c5b1ae24af4c5fdeb18a3
cosplex01 <cosplex01@naver.com>
2019-06-27 16:24:40 +0900

    부호포함 및 제외 출력옵션

---
+++ study/day4/exam6/exam6.cpp
@@ -0,0 +1,10 @@
+﻿// exam6.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int num = -1;
+	printf_s("%d, %u\n", num, num); //정수의 부호있고 없기를 출력하기 옵션
+}
