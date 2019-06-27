73f440de5ad4950a4d178558807565f5f0ccb0f1
cosplex01 <cosplex01@naver.com>
2019-06-27 16:11:54 +0900

    형의 크기지정

---
+++ study/day4/jinsuhenkan/jinsuhenkan.cpp
@@ -0,0 +1,16 @@
+﻿// jinsuhenkan.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+
+int main()
+{
+	printf_s("%.2lf\n",1.0e3);
+
+	char _c = 'A';
+
+	printf_s("%c %d\n",  _c,_c);
+	printf_s("%c %d\n", _c+1,_c+1);
+}
+
