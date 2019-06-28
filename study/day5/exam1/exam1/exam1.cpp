8a8a77d292cb80ff2a6f74a18fed4011302b3e6b
cosplex01 <cosplex01@naver.com>
2019-06-28 10:23:36 +0900

    스캔에프오류점확인

---
+++ study/day5/exam1/exam1/exam1.cpp
@@ -0,0 +1,15 @@
+﻿// exam1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <stdio.h>
+int main()
+	{
+		char a, b;
+		printf_s("문자2개 입력하세요: \n");
+		scanf_s("%c", &a,sizeof(a));
+		scanf_s("%c", &b, sizeof(b));
+		printf_s("=> %c %c \n", a, b);
+
+		return 0;
+	}
