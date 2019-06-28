378c832b3eb7e39b83708456430cd8ab44ffba5d
cosplex01 <cosplex01@naver.com>
2019-06-28 11:38:48 +0900

    분기문

---
+++ study/day5/exam1/exam3/exam3.cpp
@@ -0,0 +1,28 @@
+﻿// exam3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <stdio.h> 
+int main()
+{
+	int num = 45;
+	int _input = 32;
+	printf_s("숫자 맞추기 시도합니다,숫자를 입력해 주세요:\n");
+	scanf_s("%d",&_input);
+
+	if (num == _input) {
+		printf_s("숫자 맞추기 했습니다\n");
+	}
+	if (num != _input) {
+		printf_s("숫자 맞추기 실패했습니다\n");
+	}
+	if (num <= _input) {
+		printf_s("숫자가 큽니다\n");
+	}
+	if (num > _input) {
+		printf_s("숫자가 작습니다\n");
+	}
+	printf_s("종료합니다\n");
+
+	return 0;
+}
