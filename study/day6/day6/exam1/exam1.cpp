3b8a6e38db0f5c78d71c85531474ac0dc4bc88c6
cosplex01 <cosplex01@naver.com>
2019-07-01 10:14:44 +0900

    다중포문을 사용합니다

---
+++ study/day6/day6/exam1/exam1.cpp
@@ -0,0 +1,21 @@
+﻿// exam1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+
+int main()
+{
+	int j = 0;
+	int dan = 0;
+	for (dan = 1; dan <= 9; dan++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			printf_s("%d * %d = %d \t", dan, j, dan * j);
+		}
+		printf_s("표시줄 전환\n");
+	}
+	return 0;
+}
