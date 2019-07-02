bdd4493e09374f60fe460971f929bd4a2f23a9cb
cosplex01 <cosplex01@naver.com>
2019-07-02 09:22:50 +0900

    배열사용

---
+++ study/day6/exam1/소스.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+int main()
+{
+	int gold[5];
+
+	gold[0] = 120;
+	gold[1] = 100;
+	gold[2] = 130;
+	gold[3] = 150;
+	gold[4] = 180;
+
+	printf_s("gold[0] : %d\n",gold[0]);
+	return 0;
+}
