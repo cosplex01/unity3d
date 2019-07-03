553f526b58eeac6a0a422f1927a9e7d179029ec4
cosplex01 <cosplex01@naver.com>
2019-07-03 13:29:36 +0900

    문자열복사

---
+++ study/day7/exam5/exam5.cpp
@@ -0,0 +1,20 @@
+﻿// exam5.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+	char szBuf[256] = "헤이 와이어";
+	char szBuf2[256] = "월드월드 월드콘";
+	printf_s("문자열크기 :%d\n",strlen(szBuf));
+
+	puts(szBuf);
+	//문자열 복사 copy
+	for (size_t i = 0; i < strlen(szBuf2)+1; i++)
+	{
+		szBuf2[i] = szBuf[i];
+	}
+	puts(szBuf2);
+	return 0;
+}
