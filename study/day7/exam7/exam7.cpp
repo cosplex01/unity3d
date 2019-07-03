bfe1feef3238f1e5aed76f0a551b017f8e2656d7
cosplex01 <cosplex01@naver.com>
2019-07-03 14:35:10 +0900

    문자열을 명령어 인자로 쪼개본다

---
+++ study/day7/exam7/exam7.cpp
@@ -0,0 +1,22 @@
+﻿// exam7.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+	char _szBuf[256];
+	char* pTempNext = NULL; //문자열 기억공간용 포인터변수
+	int _bLoop = true;
+
+	// 명령어 인자1 인자2 인자3
+	while (_bLoop)
+	{
+		char _cmd[256];
+		gets_s(_szBuf,sizeof(_szBuf));
+		strcpy_s(_cmd,strtok_s(_szBuf," ",&pTempNext));
+
+
+	}
+    return 0;
+}
