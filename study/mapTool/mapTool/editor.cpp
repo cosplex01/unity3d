70276baf4f06899975b21744f9b0d617c2869dd6
cosplex01 <cosplex01@naver.com>
2019-07-05 17:19:51 +0900

    스크린버퍼를 지우고 복사본을...

--- study/mapTool/mapTool/editor.cpp
+++ study/mapTool/mapTool/editor.cpp
@@ -2,10 +2,25 @@
 
 int cursor_x = 0;
 int cursor_y = 0;
+int cursorAttr = 0xf0;
 
-void applyEditor()
+CHAR_INFO* pBackBuf;
+void changeCursorColor(int nColor)
 {
-	TGE::setCharacter(TGE::g_chiBuffer, cursor_x, cursor_y, 0x0020, 0x00f0);
+	int _Table[] = {
+		0x00,
+		0xc0,0x40,//밝은 빨간, 어두운 빨간
+		0xa0,0x20,//밝은 녹색, 어두운 녹색
+		0x90,0x10, //밝은 파란, 어두운 파란
+		0xf0,0x80 //밝은 빨강, 어두운 빨강
+	};
+	cursorAttr = _Table[nColor];
+}
+
+void applyEditor(HANDLE hStdout)
+{
+	TGE::clearScreenBuffer(0x0020, 0x00f0); //화면버퍼 클리어
+	//TGE::setCharacter(TGE::g_chiBuffer, cursor_x, cursor_y, 0x0020, 0x00f0); //커서선택하여 그리기
 
 	if (TGE::input::g_KeyTable[VK_UP]) {
 		cursor_y--;
@@ -23,4 +38,10 @@ void applyEditor()
 		cursor_x++;
 		TGE::input::g_KeyTable[VK_RIGHT] = false;
 	}
+	if (TGE::input::g_KeyTable[VK_SPACE]) {
+		TGE::setCharacter(TGE::g_chiBuffer, cursor_x, cursor_y, 0x00c0, cursorAttr); //커서선택하여 그리기
+		TGE::input::g_KeyTable[VK_SPACE] = false;
+	}
+	TGE::copyScreenBuffer(TGE::g_chiBuffer, pBackBuf); //가상 버퍼에 스크린버퍼 복사해둠[더블 버퍼링 트릭]
+	TGE::setCharacter(TGE::g_chiBuffer, cursor_x, cursor_y, 0x00c0, cursorAttr); //커서선택하여 그리기
 }
--- study/mapTool/mapTool/mapTool.cpp
+++ study/mapTool/mapTool/mapTool.cpp
@@ -2,7 +2,10 @@
 //
 #include "..\..\..\cstudy\engine\tge.h"
 #include <iostream>
-void applyEditor();
+
+extern CHAR_INFO* pBackBuf; 
+void applyEditor(HANDLE hStdout);
+void changeCursorColor(int Color);
 
 int main()
 {
@@ -11,23 +14,43 @@ int main()
 	HANDLE hStdout;
 	TGE::startTGE(&hStdout);
 
-	TGE::clearScreenBuffer(0x0020, 0x0090);//밝은파란색으로 배경처리를 한다
+	pBackBuf = TGE::CreateScreenBuffer(); // 맵상에서 계속 존재시킬것을 위한 임시버퍼생성
+	TGE::clearScreenBuffer(pBackBuf, 0x0020, 0x0090); //가상버퍼를 클리어시킨다. 처음에
 	//전체 맵사이즈는 80*25 이다
 	while (bLoop)
 	{
-		/*
-		char szBuf[256];
-		gets_s(szBuf);
-		int tokenNum = TGE::Tokenize(szBuf);
-		if (!strcmp("exit", TGE::g_szTokens[0])) {
-			bLoop = false;
-			puts("프로그램 종료");
+		if (TGE::input::g_KeyTable[VK_RETURN]) {
+			//동기식 입력처리
+			TGE::input::pauseInputThread();
+			TGE::input::setEditMode();
+
+			TGE::showCursor(hStdout);
+			TGE::setCursor(hStdout, 0, 20);
+
+			char szBuf[256];
+			gets_s(szBuf);
+			int tokenNum = TGE::Tokenize(szBuf);
+			if (!strcmp("exit", TGE::g_szTokens[0])) {
+				bLoop = false;
+				puts("프로그램 종료");
+			}
+			else if(!strcmp("chgcolor", TGE::g_szTokens[0])) {
+				changeCursorColor(
+					atoi(TGE::g_szTokens[0])
+				);
+				puts("color");
+			}
+			else {
+				puts("알수없는 명령어입니다");
+			}
+			//커서 다시 숨기기
+			TGE::hideCursor(hStdout);
+			TGE::input::g_KeyTable[VK_RETURN] = false;
+
+			TGE::input::setEditMode();
+			TGE::input::resumeInputThread();
 		}
-		else {
-			puts("알수없는 명령어입니다");
-		}
-		*/
-		applyEditor();
+		applyEditor(hStdout);
 		TGE::updateBuffer(hStdout, TGE::g_chiBuffer);
 	}
 	TGE::endTGE();
