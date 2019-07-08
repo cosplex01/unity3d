a0a4736d7f96dcd473e72d1cc4579a11d99591ad
cosplex01 <cosplex01@naver.com>
2019-07-08 16:31:34 +0900

    일부수정된 거에요

---
+++ study/day10/exam5/exam5.cpp
@@ -0,0 +1,21 @@
+﻿// exam5.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	char* ptr = NULL;
+	ptr = (char*)malloc(256); //실행중에 동적인 메모리관리함
+
+	if (ptr != NULL) {
+		gets_s(ptr, 256);
+		printf_s("%s\n", ptr);
+		free(ptr); //메모리 복귀시킴
+	}
+	else {
+		printf_s("메모리확보 실패\n");
+	}
+	
+  return 0;
+}
