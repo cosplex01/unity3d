853d860864d0abf6ab369d899462f5f74263d1fe
cosplex01 <cosplex01@naver.com>
2019-07-09 11:15:23 +0900

    외부파일을 사용해서 함수를 줄이자

---
+++ study/day11/exam3/exam3.cpp
@@ -0,0 +1,16 @@
+﻿// exam3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include <iostream>
+#include "gameobject.h"
+
+int main()
+{
+	S_gameObject* pObject;
+	pObject = (S_gameObject*)malloc(sizeof(S_gameObject));
+	const char* szName = "war★nurse쿠루미";
+
+	initGmaeObject(pObject,szName,strlen(szName));
+	printf_s("%s -- %f -- %d\n", pObject->m_szName, pObject->m_fXpos, pObject->m_nDamage);
+	releseGameObject(pObject);
+    return 0;
+}
