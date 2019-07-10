c95e9181df9552aa4ece3310f291baaaa7822650
cosplex01 <cosplex01@naver.com>
2019-07-10 11:08:49 +0900

    외부파일에서 구조체 파일을 읽어옵니다

---
+++ study/day12/exam3/exam3.cpp
@@ -0,0 +1,28 @@
+﻿// exam3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include <iostream>
+struct S_GameObject {
+	int m_nSkima;
+	double m_xPos, m_yPos;
+	int m_nld;
+	double m_nSpeed;
+};
+
+int main()
+{
+	FILE* fp;
+	fopen_s(&fp,"puransinu.dat","r");
+
+	if (fp) {
+		puts("파일오픈 성공");
+		S_GameObject* pObj = (S_GameObject*)malloc(sizeof(S_GameObject));
+
+		fread_s(pObj, sizeof(S_GameObject), sizeof(S_GameObject), 1, fp);
+		printf_s("%lf,%lf,%d,%d,\n", pObj->m_xPos, pObj->m_yPos, pObj->m_nSkima, pObj->m_nld);
+
+		fclose(fp);
+		free(pObj);
+	}
+	
+  return 0;
+}
